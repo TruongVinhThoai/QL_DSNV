d1305e4f038a2c26f3a0d0eb9150172355cae1ab
truongthoai <thoaitv3107@gmail.com>
2023-08-14 20:39:15 +0700

    macOS

---
+++ js/model.js
@@ -0,0 +1,19 @@
+function Employee(
+  idEmployee,
+  nameEmployee,
+  emailEmployee,
+  passEmployee,
+  datepickerEmployee,
+  salaryEmployee,
+  positionEmployee,
+  timeEmployee
+) {
+  this.id = idEmployee;
+  this.name = nameEmployee;
+  this.email = emailEmployee;
+  this.pass = passEmployee;
+  this.datepicker = datepickerEmployee;
+  this.salary = salaryEmployee;
+  this.position = positionEmployee;
+  this.time = timeEmployee;
+}
